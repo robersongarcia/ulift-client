fb14324b01ed344ab3ccb7a2682cd579b6a6aa24
Roberson Garcia <roberson.gar12@gmail.com>
2023-04-21 02:56:51 -0400

    linter setup and file cleanup

---
+++ src/UliftApp.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material'
+
+export default function UliftApp (): JSX.Element {
+  return (
+    <>
+      <Typography variant='h1' component='h1'>Ulift</Typography>
+      <Typography variant='h5' component='h2'>Si</Typography>
+    </>
+  )
+}
