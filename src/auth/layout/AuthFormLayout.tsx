afdff64ad26ce67e51f6aaf5ac944034a61e9bc2
Roberson Garcia <roberson.gar12@gmail.com>
2023-05-04 01:36:11 -0400

    login page styles created

---
+++ src/auth/layout/AuthFormLayout.tsx
@@ -0,0 +1,51 @@
+import { Grid } from '@mui/material'
+import { type PropsWithChildren } from 'react'
+
+export function AuthFormLayout ({ children }: PropsWithChildren): JSX.Element {
+  return (
+    <Grid
+        // general Grid
+        container
+        component='main'
+        spacing={0}
+        direction='row'
+        justifyContent='center'
+        alignItems='center'
+        sx={{
+          minHeight: '100vh',
+          maxHeight: '100vh',
+          padding: 4,
+          backgroundImage: `url(${'/background.jpg'})`,
+          // cover the image with a dark layer with a 50% opacity
+          // //backgroundBlendMode: 'multiply',
+          // backgroundColor: 'rgba(64, 180, 229, 0.5)',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover'
+        }}
+    >
+        <Grid
+            // form grid card
+            container
+            direction='column'
+            justifyContent='center'
+            alignItems='center'
+            sx={{
+              backgroundColor: (theme) => theme.palette.primary.main,
+              padding: 3,
+              borderRadius: 3,
+              width: {
+                xs: '100%',
+                sm: '80%',
+                md: '60%'
+              },
+              minHeight: '50%',
+              boxShadow: '0px 5px 5px rgba(0, 0, 0, 0.5)',
+              color: (theme) => theme.palette.primary.contrastText
+            }}
+        >
+            <img srcSet='/logo-name.png' alt='ulift application logo'/>
+            {children}
+        </Grid>
+    </Grid>
+  )
+}
--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,8 +1,124 @@
+import { AuthFormLayout } from '../layout/AuthFormLayout'
+import { Grid, TextField, Button, Link } from '@mui/material'
+import { Link as RouterLink } from 'react-router-dom'
 
 export function LoginPage (): JSX.Element {
   return (
-    <>
-        <h1>Login Page</h1>
-    </>
+    <AuthFormLayout>
+      <Grid container sx={{ mt: 3 }}>
+          <Grid item
+              xs={12}
+              sx={{
+                mb: 2,
+                borderRadius: 1
+              }}
+                >
+            <TextField
+              label="Correo"
+              type="email"
+              fullWidth
+              name="email"
+              placeholder='correo@ucab.edu.ve'
+              InputProps={{
+                sx: {
+                  '& input': {
+                    color: (theme) => theme.palette.primary.contrastText
+                  }
+                }
+              }}
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: (theme) => theme.palette.primary.contrastText
+                },
+                '& .MuiInputLabel-root': {
+                  color: (theme) => theme.palette.primary.contrastText
+                },
+                '& .MuiFormLabel-root': {
+                  color: (theme) => theme.palette.primary.contrastText
+                },
+                '& .MuiFormLabel-root.Mui-focused': {
+                  color: (theme) => theme.palette.primary.contrastText
+                }
+              }}
+              InputLabelProps={{
+                sx: {
+                  color: (theme) => theme.palette.primary.contrastText
+                }
+              }}
+              variant='filled'
+            />
+          </Grid>
+          <Grid
+            item xs={12}
+            sx={{
+              mb: 2,
+              borderRadius: 1
+            }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              fullWidth
+              name="password"
+              InputProps={{
+                sx: {
+                  '& input': {
+                    color: (theme) => theme.palette.primary.contrastText
+                  }
+                }
+              }}
+              sx={{
+                '& .MuiInputBase-root': {
+                  color: (theme) => theme.palette.primary.contrastText
+                },
+                '& .MuiInputLabel-root': {
+                  color: (theme) => theme.palette.primary.contrastText
+                },
+                '& .MuiFormLabel-root': {
+                  color: (theme) => theme.palette.primary.contrastText
+                },
+                '& .MuiFormLabel-root.Mui-focused': {
+                  color: (theme) => theme.palette.primary.contrastText
+                }
+              }}
+              InputLabelProps={{
+                sx: {
+                  color: (theme) => theme.palette.primary.contrastText
+                }
+              }}
+              variant='filled'
+            />
+          </Grid>
+
+          <Grid container
+            direction='row'
+            justifyContent='center'
+            sx={{ mb: 2 }}>
+
+            <Grid item xs={12} sm={6} >
+              <Button variant="outlined" fullWidth type="submit" sx={
+                {
+                  color: (theme) => theme.palette.primary.contrastText,
+                  borderColor: (theme) =>
+                    theme.palette.primary.contrastText,
+                  '&:hover': {
+                    backgroundColor: (theme) =>
+                      theme.palette.primary.contrastText,
+                    color: (theme) => theme.palette.primary.main
+                  }
+                }
+              }>
+                Login
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Link component={RouterLink} color="inherit" to="/auth/registro">
+              Crear una cuenta
+            </Link>
+          </Grid>
+
+        </Grid>
+    </AuthFormLayout>
   )
 }
